d2279ebe5242c60c133050d9ba1f69046f751ae9
carlos <carlosh10@gmail.com>
2015-05-17 18:35:41 -0300

    devise fix try

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "carlos@comex.guru"
+  config.mailer_sender = "carlosh10@gmail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
