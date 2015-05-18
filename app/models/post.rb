edc94c55b4038baa64abe8679774d7e1fb4db5df
carlos <carlosh10@gmail.com>
2015-05-17 22:19:38 -0300

    completed validation checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,9 @@ class Post < ActiveRecord::Base
   scope :ordered_by_title, -> {order('title DESC')}
   scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
 
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {minimum: 20}, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+
 end
