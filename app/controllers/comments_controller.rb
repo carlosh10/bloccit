2502053ae9191f72d960baab437bc8f68ecc97ba
carlos <carlosh10@gmail.com>
2015-06-28 16:03:31 -0300

    finished create with ajax cp

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,21 +35,31 @@ class CommentsController < ApplicationController
 
       def create
         @post = Post.find(params[:post_id])
-         @comment = Comment.new(params.require(:comment).permit(:body))
-         authorize @comment
-         @comment.user = current_user
-         @comment.post = @post
+        @comments = @post.comments
+
+        @comment = current_user.comments.build( comment_params )
+        @comment.post = @post
+        @new_comment = Comment.new
+
+        authorize @comment
 
         if @comment.save
-          flash[:notice] = "Post was saved."
-          redirect_to [@post.topic, @post]
-
+          flash[:notice] = "Comment was created."
         else
-           flash[:error] = "There was an error saving the post. Please try again."
-           redirect_to [@post.topic, @post]
+          flash[:error] = "There was an error saving the comment. Please try again."
+        end
+
+        respond_to do |format|
+          format.html
+          format.js
         end
       end
+  
+  private
 
+      def comment_params
+        params.require(:comment).permit(:body)
+      end
 end
 
 
